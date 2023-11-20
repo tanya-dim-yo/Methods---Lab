0fd03682f3d61aee424b50d22b3dbb4893fbee46
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-20 05:07:04 +0200

    Add files via upload

---
+++ 05. Orders/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _05.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int productQuantity = int.Parse(Console.ReadLine());
+
+            OrderSum(product, productQuantity);
+        }
+
+        private static void OrderSum(string product, int productQuantity)
+        {
+            double orderSum = 0;
+
+            switch (product)
+            {
+                case "coffee":
+                    orderSum = productQuantity * 1.50;
+                    break;
+                case "water":
+                    orderSum = productQuantity * 1.0;
+                    break;
+                case "coke":
+                    orderSum = productQuantity * 1.40;
+                    break;
+                case "snacks":
+                    orderSum = productQuantity * 2.0;
+                    break;
+            }
+
+            Console.WriteLine($"{orderSum:F2}");
+        }
+    }
+}
