aa42440f5c1834d9c2a983d30f8afec1a3de7d80
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-20 05:06:15 +0200

    Add files via upload

---
+++ 04. Printing Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04.Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            PrintingTriangle(rows);
+        }
+
+        private static void PrintingTriangle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            { 
+                PrintingLine(1, i);
+            }
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+                PrintingLine(1, i);
+            }
+        }
+
+        private static void PrintingLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
