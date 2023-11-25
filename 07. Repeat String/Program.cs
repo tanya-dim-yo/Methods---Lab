b706d37abd8bb9751eacd8b1738c8f07b1455e73
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-25 04:53:25 +0200

    Add files via upload

---
+++ 07. Repeat String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace _07.Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = Console.ReadLine();
+            int timesToRepeatString = int.Parse(Console.ReadLine());
+            string repeatedString = RepeatString(myString, timesToRepeatString);
+            Console.WriteLine(repeatedString);
+        }
+
+        private static string RepeatString(string myString, int timesToRepeatString)
+        {
+            StringBuilder repeatedString = new StringBuilder();
+            for (int i = 0; i < timesToRepeatString; i++)
+            {
+                repeatedString.Append(myString);
+            }
+            return repeatedString.ToString();
+        }
+    }
+}
