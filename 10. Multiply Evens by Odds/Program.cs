e8cb1ef5120ddd29a694bb1b8e1069b1ebed8656
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-25 04:55:17 +0200

    Add files via upload

---
+++ 10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _10.Multiply_Evens_By_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputNumber = Console.ReadLine();
+            int sumEven = GetSum(inputNumber, true);
+            int sumOdd = GetSum(inputNumber, false);
+            int result = sumEven * sumOdd;
+            Console.WriteLine(result);
+        }
+
+        private static int GetSum(string numbers, bool isEven)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] >= 48 && numbers[i] <= 57)
+                {
+                    int currentNumber = int.Parse(numbers[i].ToString());
+
+                    if (currentNumber % 2 == 0 && isEven)
+                    {
+                        sum += currentNumber;
+                    }
+                    else if (currentNumber % 2 != 0 && !isEven)
+                    {
+                        sum += currentNumber;
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
