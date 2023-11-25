76014a7c92508aeebe97089e8b4167168ade51e2
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-25 04:54:02 +0200

    Add files via upload

---
+++ 08. Math Power/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _08.Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            double result = MathPower(number, power);
+            Console.WriteLine(result);
+        }
+
+        private static double MathPower(double n, double power)
+        {
+            double result = 1;
+
+            for (int i = 0; i < power; i++)
+            {
+                result *= n;
+            }
+
+            return result;
+        }
+    }
+}
