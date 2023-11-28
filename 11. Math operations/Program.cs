c972a361cde3c3462945646a8598c82df78f7354
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-28 23:50:13 +0200

    Add files via upload

---
+++ 11. Math operations/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _11.Math_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double secondNumber = double.Parse(Console.ReadLine());
+            double result = MathCalculation(firstNumber, operation, secondNumber);
+            Console.WriteLine(result);
+        }
+
+        private static double MathCalculation(double a, string @operator, double b)
+        {
+            double result = 0;
+
+            switch (@operator)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
